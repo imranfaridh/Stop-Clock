926de1f752300742466e3b16146e54029a8a2cc1
imranfaridh <113770763+imranfaridh@users.noreply.github.com>
2023-06-12 08:06:58 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,61 @@
+const btnStart = document.querySelector('.start');
+const btnStop = document.querySelector('.stop');
+const btnReset = document.querySelector('.reset');
+
+let hrs=min=sec=ms=0;
+
+btnStart.addEventListener('click',()=>{
+
+    btnStart.classList.add('start-active');
+    btnStop.classList.remove('stop-active');
+
+    starTimer = setInterval(()=>{
+        ms++;
+        if(ms==100){
+            sec++;
+            ms=0;
+        }
+        if(sec==60){
+            min++;
+            sec=0;
+        }
+        if(min==60){
+            hrs++;
+            min=0;
+        }
+        updateDisplay()
+    },10);
+});
+
+btnStop.addEventListener('click',()=>{
+
+    btnStart.classList.remove('start-active');
+    btnStop.classList.add('stop-active');
+
+  clearInterval(starTimer);
+});
+
+btnReset.addEventListener('click',()=>{
+
+    btnStart.classList.remove('start-active');
+    btnStop.classList.remove('stop-active');
+
+    hrs=min=sec=ms=0;
+    clearInterval(starTimer);
+    updateDisplay();
+});
+
+function updateDisplay(){
+    // Formatted Display
+    phrs=hrs<10?'0'+hrs:hrs;
+    pmin=min<10?'0'+min:min;
+    psec=sec<10?'0'+sec:sec;
+    pms=ms<10?'0'+ms:ms;
+
+    //Output
+    document.querySelector('.hrs').innerText=phrs;
+    document.querySelector('.min').innerText=pmin;
+    document.querySelector('.sec').innerText=psec;
+    document.querySelector('.ms').innerText=pms;
+
+}
